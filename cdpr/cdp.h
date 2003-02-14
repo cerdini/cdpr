5b649f18072504c22f1a0b036f91f03543b836de
ronnocol <>
2003-02-14 05:21:13 +0000

    Initial Import

---
+++ cdpr/cdp.h
@@ -0,0 +1,70 @@
+/*
+* cdpr - Cisco Discovery Protocol Reporter
+* Copyright <c) 2002 MonkeyMental.com
+*
+* This program will show you which Cisco device your machine is
+* connected to based on CDP packets received.
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <netinet/in.h>
+
+/* Define the constants and text values for the 'type' field: */
+#define TYPE_DEVICE_ID			0x0001
+#define TYPE_ADDRESS			0x0002
+#define TYPE_PORT_ID			0x0003
+#define TYPE_CAPABILITIES		0x0004
+#define TYPE_IOS_VERSION		0x0005
+#define TYPE_PLATFORM			0x0006
+#define TYPE_IP_PREFIX			0x0007
+
+#define TYPE_VTP_MGMT_DOMAIN	0x0009
+#define TYPE_NATIVE_VLAN		0x000a
+#define TYPE_DUPLEX				0x000b
+
+struct
+{
+    int type;
+    char *val;
+}
+type_vals[] = {
+	{ TYPE_DEVICE_ID,       "Device ID" },
+	{ TYPE_ADDRESS,         "Addresses" },
+	{ TYPE_PORT_ID,         "Port ID" },
+	{ TYPE_CAPABILITIES,    "Capabilities" },
+	{ TYPE_IOS_VERSION,     "Software version" },
+	{ TYPE_PLATFORM,        "Platform" },
+	{ TYPE_IP_PREFIX,       "IP Prefix (used for ODR)" },
+	{ TYPE_VTP_MGMT_DOMAIN, "VTP Management Domain" },
+	{ TYPE_NATIVE_VLAN,     "Native VLAN" },
+	{ TYPE_DUPLEX,          "Duplex" },
+	{ 0,                    NULL },
+};
+
+typedef struct _cdp_packet_header
+{
+	u_int8_t  version;		// always one
+	u_int8_t  time_to_live;	// in seconds
+	u_int16_t checksum;		// ip checksum
+} CDP_HDR;
+
+typedef struct _cfp_packet_data
+{
+	u_int16_t type;			// see TYPE_ above
+	u_int16_t length;		// total length of type/length/value
+	//        value;		// variable length value
+} CDP_DATA;
+
---
+++ cdpr/cdpr.c
@@ -0,0 +1,436 @@
+/*
+* cdpr - Cisco Discovery Protocol Reporter
+* Copyright <c) 2002 MonkeyMental.com
+*
+* This program will show you which Cisco device your machine is
+* connected to based on CDP packets received.
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <pcap.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "cdp.h"
+
+
+void
+dump_ip (const u_char *ip, int len)
+{
+	printf ("%d.%d.%d.%d",
+		(int) ip[0], (int) ip[1], (int) ip[2], (int) ip[3]);
+}
+
+void
+dump_hex (const u_char *p, int len)
+{
+	while (len--)
+	{
+		printf ("%02X ", *p++);
+	}
+}
+
+void
+dump_ascii (const u_char *p, int len)
+{
+	while (len--)
+	{
+		printf ("%c", (*p < ' ' || *p > '~') ? '.' : *p);
+		++p;
+	}
+}
+
+void
+dump_data (const u_char *p, int len)
+{
+	int i;
+
+	for (i = 0; i < len; i += 16, p += 16)
+	{
+		printf ("%p:  ", p);
+		dump_hex (p, 16);
+		printf (" ");
+		dump_ascii (p, 16);
+		printf ("\n");
+	}
+}
+char *
+get_cdp_type (int type)
+{
+    int i;
+
+    for (i = 0; type_vals[i].type != 0; ++i)
+    {
+    	if (type == type_vals[i].type)
+    	{
+    		return type_vals[i].val;
+    	}
+    }
+    return "Unknown type";
+}
+
+void
+print_cdp_address (u_char *v, int vlen, int verbose)
+{
+	int i;
+	u_int32_t number = ntohl (*((u_int32_t *) v));
+
+	if (verbose > 0)
+	{
+		printf ("  number: %d\n", number);
+	}
+
+	v += sizeof (u_int32_t);
+	for (i = 0; i < number; ++i)
+	{
+		u_char protocol      = *v;
+		u_char protocol_len  = *(v+1);
+		u_char *protocol_val = v+2;
+		int    address_len   = ntohs(*((u_int16_t *)(v+2+protocol_len)));
+		u_char *address_val  = v+2+protocol_len+2;
+		
+		if (verbose > 0)
+		{
+			printf ("  protocol: %02x\n", protocol);
+			printf ("  protocol len: %02x\n", protocol_len);
+			printf ("  protocol val: ");
+			dump_hex (protocol_val, protocol_len);
+			printf ("\n");
+		
+			printf ("  address len: %02x\n", address_len);
+			printf ("  address val: ");
+		}
+		else
+		{
+			printf ("  value:  ");
+		}
+		if (protocol_len == 1 && *protocol_val == 0xCC && address_len == 4)
+			dump_ip (address_val, address_len);
+		else
+			dump_hex (address_val, address_len);
+		printf ("\n");
+
+		v += (2+protocol_len+2+address_len);
+	}
+}
+
+void
+print_cdp_capabilities (u_char *v, int vlen)
+{
+	u_int32_t cap = ntohl (*((u_int32_t *) v));
+
+	printf ("  value:  %08x\n", cap);
+	if (cap & 0x01) printf ("          Performs level 3 routing for at least one network layer protocol.\n");
+	if (cap & 0x02) printf ("          Performs level 2 transparent bridging.\n");
+	if (cap & 0x04) printf ("          Performs level 2 source-route bridging.\n");
+	if (cap & 0x08) printf ("          Performs level 2 switching.\n");
+	if (cap & 0x10) printf ("          Sends and receives packets for at least one network layer protocol.\n");
+	if (cap & 0x20) printf ("          The bridge or switch does not forward IGMP Report packets on nonrouter ports.\n");
+	if (cap & 0x40) printf ("          Provides level 1 functionality.\n");
+}
+
+void
+print_cdp_packet (const u_char *p, int plen, int verbose)
+{
+	CDP_HDR *h;
+	CDP_DATA *d;
+
+	h = (CDP_HDR *) p;
+
+	// dump_data (p, 128);
+
+	if (verbose > 1 )
+	{
+		printf ("\ncdp packet:\n");
+		printf ("  version:      %02x\n", h->version);
+		printf ("  time to live: %02x\n", h->time_to_live);
+		printf ("  checksum:     %04x\n", ntohs (h->checksum));
+	}
+
+	d = (CDP_DATA *) (p + sizeof (CDP_HDR));
+	plen -= sizeof (CDP_HDR);
+
+	while (plen > sizeof (CDP_DATA))
+	{
+		int type = ntohs (d->type);
+		int length = ntohs (d->length);
+		u_char *v = (u_char *) d + sizeof (CDP_DATA);
+		int vlen = length - sizeof (CDP_DATA);
+		
+
+		//	printf ("\ncdp type/len/val:\n");
+		if(verbose > 0 )
+		{
+			printf ("  type:   %04x - %s\n", type, get_cdp_type (type));
+		}
+		if(verbose > 0)
+		{
+			printf ("  length: %04x\n", length);
+		}
+		switch (type)
+		{
+		case TYPE_DEVICE_ID:
+			printf ("%s\n", get_cdp_type (type));
+		    printf ("  value:  %.*s\n", vlen, v);
+			break;
+
+		case TYPE_ADDRESS:
+			printf ("%s\n", get_cdp_type (type));
+			print_cdp_address (v, vlen, verbose);
+			break;
+
+		case TYPE_PORT_ID:
+			printf ("%s\n", get_cdp_type (type));
+			printf ("  value:  %.*s\n", vlen, v);
+			break;
+
+		case TYPE_CAPABILITIES:
+			if(verbose > 0)
+			{
+				printf ("%s\n", get_cdp_type (type));
+				print_cdp_capabilities (v, vlen);
+			}
+			break;
+
+		case TYPE_IOS_VERSION:
+			if(verbose > 0)
+			{
+				printf ("%s\n", get_cdp_type (type));
+				printf ("  value:  %.*s\n", vlen, v);
+			}
+			break;
+
+		case TYPE_PLATFORM:
+			if(verbose > 0)
+			{
+				printf ("%s\n", get_cdp_type (type));
+				printf ("  value:  %.*s\n", vlen, v);
+			}
+			break;
+
+		case TYPE_IP_PREFIX:
+			if(verbose > 0)
+			{
+				printf ("%s\n", get_cdp_type (type));
+				printf ("  value:  ");
+				dump_hex (v, vlen);
+				printf ("\n");
+			}
+			break;
+
+		case TYPE_VTP_MGMT_DOMAIN:
+			if(verbose > 0)
+			{
+				printf ("%s\n", get_cdp_type (type));
+				printf ("  value:  %.*s\n", vlen, v);
+			}
+			break;
+
+		case TYPE_NATIVE_VLAN:
+			if(verbose > 0)
+			{
+				printf ("%s\n", get_cdp_type (type));
+				printf ("  value:  ");
+				dump_hex (v, vlen);
+				printf ("\n");
+			}
+			break;
+		
+		case TYPE_DUPLEX:
+			if(verbose > 0)
+			{
+				printf ("%s\n", get_cdp_type (type));
+				printf ("  value:  ");
+				dump_hex (v, vlen);
+				printf ("\n");
+			}
+			break;
+
+		default:
+			if(verbose > 1)
+			{
+				printf ("%s\n", get_cdp_type (type));
+				printf ("  value:  ");
+				dump_hex (v, vlen);
+				printf ("\n");
+			}
+		}
+
+	    plen -= length;
+	    d = (CDP_DATA *) ((u_char *) d + length);
+	}
+}
+
+
+int
+print_dlt(pcap_t *handle)
+{
+	int type;
+
+	/* Print the DLL Type*/
+	switch(type = pcap_datalink(handle))
+	{
+		case DLT_NULL:
+			printf("Data Link Type: BSD Loopback encapsulation.\n");
+			break;
+		case DLT_EN10MB:
+			printf("Data Link Type: Ethernet (10MB, 100MB, 1000MB and up).\n");
+			break;
+		case DLT_IEEE802:
+			printf("Data Link Type: IEEE 802.5 Token Ring.\n");
+			break;
+		case DLT_ARCNET:
+			printf("Data Link Type: ARCNET.\n");
+			break;
+		case DLT_PPP:
+			printf("Data Link Type: PPP (Point-to-Point Protocol).\n");
+			break;
+		case DLT_FDDI:
+			printf("Data Link Type: FDDI.\n");
+			break;
+		case DLT_ATM_RFC1483:
+			printf("Data Link Type: RFC 1483 LLC/SNAP-encapsulated ATM.\n");
+			break;
+		case DLT_RAW:
+			printf("Data Link Type: raw IP.\n");
+			break;
+		case DLT_PPP_SERIAL:
+			printf("Data Link Type: PPP in HDLC-like framing.\n");
+			break;
+		case DLT_PPP_ETHER:
+			printf("Data Link Type: PPPoE.\n");
+			break;
+		case DLT_C_HDLC:
+			printf("Data Link Type: Cisco PPP with HDLC framing.\n");
+			break;
+		case DLT_IEEE802_11:
+			printf("Data Link Type: IEEE 802.11 wireless LAN.\n");
+			break;
+		case DLT_LOOP:
+			printf("Data Link Type: OpenBSD loopback encapsulation.\n");
+			break;
+		case DLT_LTALK:
+			printf("Data Link Type: Apple LocalTalk.\n");
+			break;
+		default:
+			printf("%d is an unknown Data Link Transport\n", type);
+	}
+
+	return 0;
+}
+
+int
+usage(void)
+{
+	puts("d: Specify device to use (eth0, hme0, etc.)");
+	puts("v: Set verbose mode");
+	puts("h: Print this usage");
+
+	exit(0);
+}
+
+int
+main(int argc, char *argv[])
+{
+	pcap_t *handle;										/* Session handle */
+	char *dev;											/* Find a device */
+	char errbuf[PCAP_ERRBUF_SIZE]="";					/* Error string */
+	struct bpf_program filter;							/* The compiled filter expression */
+	char filter_app[] = "ether host 01:00:0c:cc:cc:cc";	/* The filter expression */
+	bpf_u_int32 mask;									/* The netmask of our sniffing device */
+	bpf_u_int32 net;									/* The IP of our sniffing device */
+	struct pcap_pkthdr header;							/* The header that pcap gives us */
+	const u_char *packet;								/* The actual packet */
+	char version[] = "1.0.0";							/* Version Number */
+
+	int c,sd=0,verbose=0;								/* Used for getopt, and spec. verbose lvl */
+
+
+	/* Print out header */
+	printf("cdpr - Cisco Discovery Protocol Reporter Version %s\n", version);
+	printf("Copyright (c) 2002 - MonkeyMental.com\n\n");
+
+	/* Check command-line options */
+	while((c = getopt(argc, argv, "d:vh")) !=EOF)
+		switch(c)
+		{
+			case 'd':
+				sd = 1;
+				dev = optarg;
+				break;
+			case 'v':
+				verbose++;
+				break;
+			case 'h':
+			case '?':
+				usage();
+				break;
+		}
+
+	/* Get a pcap capable device */
+	if(sd != 1)
+	{
+		if((dev = pcap_lookupdev(errbuf)) == NULL)
+		{
+			printf("Error finding device (%s)\n", errbuf);
+			exit(1);
+		}
+	}
+
+    printf("Using Device: %s\n", dev);
+
+	/* Get the network number and netmask */
+	pcap_lookupnet(dev, &net, &mask, errbuf);
+
+	/* Open the pcap device */
+	if((handle = pcap_open_live(dev, BUFSIZ, 1, 0, errbuf)) == NULL)
+	{
+		printf("Error opening device (%s)\n", errbuf);
+		exit(1);
+	}
+	else if (strlen(errbuf))
+	{
+		printf("Warning opening device (%s)\n", errbuf);
+	}
+
+	/* Compile the pcap filter */
+	pcap_compile(handle, &filter, filter_app, 0, net);
+
+	/* Activate the pcap filter */
+	pcap_setfilter(handle, &filter);
+
+	/* Get the next packet that comes in, we only need one */
+	printf("Waiting for CDP advertisement, default config is to transmit CDP packets every 60 seconds\n");
+	packet = pcap_next(handle, &header);
+
+	/* Print its length */
+	if(verbose > 0)
+	{
+		printf("Received a CDP packet, header length: %d\n", header.len);
+	}
+
+	// print cdp packet, 22 bytes into packet
+	print_cdp_packet (packet+22, header.len-22, verbose);
+
+	if (verbose > 1)
+	{
+		print_dlt(handle);
+	}
+	
+	pcap_close(handle);
+	return(0);
+}
